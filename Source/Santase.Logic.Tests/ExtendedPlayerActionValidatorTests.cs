72e8ced1080644fc38306ea740d077202583bd22
shakuu <todorov.h@gmail.com>
2016-07-23 01:50:02 +0300

    update: task3

--- Source/Santase.Logic.Tests/ExtendedPlayerActionValidatorTests.cs
+++ Source/Santase.Logic.Tests/ExtendedPlayerActionValidatorTests.cs
@@ -75,5 +75,38 @@
 
             Assert.IsTrue(actualOutcome);
         }
+
+        [Test]
+        public void IsValid_ShouldReturnTrue_IfPlayerActionIsChangeTrumpAndArgumentsAreCorrect()
+        {
+            var playerActionValidator = new PlayerActionValidator();
+
+            var deck = new Deck();
+
+            var stateManager = new StateManager();
+            var context = new PlayerTurnContext(
+                new MoreThanTwoCardsLeftRoundState(stateManager),
+                deck.TrumpCard,
+                deck.CardsLeft,
+                30,
+                30);
+
+            var testPlayerHand = new List<Card>()
+            {
+                new Card(CardSuit.Club, CardType.Queen),
+                new Card(CardSuit.Diamond, CardType.King),
+                new Card(CardSuit.Club, CardType.King),
+                new Card(deck.TrumpCard.Suit, CardType.Nine)
+            };
+
+            var playerActionCard = new Card(CardSuit.Club, CardType.King);
+
+            var actualOutcome = playerActionValidator.IsValid(
+                PlayerAction.ChangeTrump(),
+                context,
+                testPlayerHand);
+
+            Assert.IsTrue(actualOutcome);
+        }
     }
 }
